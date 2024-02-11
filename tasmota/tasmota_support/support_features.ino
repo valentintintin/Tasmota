b54081708f912c62444d1554ab35fda3de6e0fd4
Valentin Saugnier <valentin.s.10@gmail.com>
2024-02-11 13:56:23 +0100

    Merge of main

--- tasmota/tasmota_support/support_features.ino
+++ tasmota/tasmota_support/support_features.ino
@@ -173,7 +173,7 @@ constexpr uint32_t feature[] = {
   0x00010000 |  // xdrv_17_rcswitch.ino
 #endif
 #ifdef USE_DIO_REMOTE
-    feature2 |= 0x00010000;  // xdrv_200_dioremote.ino
+    0x00010000 |  // xdrv_200_dioremote.ino
 #endif
 #if defined(USE_LIGHT) && defined(USE_ARMTRONIX_DIMMERS)
   0x00020000 |  // xdrv_18_armtronixdimmer.ino
